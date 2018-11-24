346bf93c2ac3b031efcac4f41b7daf0050a67356
logan <eagle.cfeng@gmail.com>
2018-11-24 17:45:12 -0500

    Update final_webScraper.py

--- final_webScraper.py
+++ final_webScraper.py
@@ -1,23 +1,17 @@
 # coding: utf-8
 import io
 import csv
+import codecs
 from urllib.request import urlopen as uo
 from bs4 import BeautifulSoup as soup
 from tkinter import *
 
-productName_list =[]
-DropDown_list =[]
-oriPrice_list=[]
-nowPrice_list =[]
-plate_list=[]
-ratePercent_list=[]
-user_list=[]
-
-containerAmount = 0
-def getURL():
-    for counter in range(1, 29):
+def getURL(page):
+    global containerAmount
+    for counter in range(page):
         my_url = 'https://store.steampowered.com/search/?specials=1&page=' + str(counter)
         print(counter)
+        
         uPage = uo(my_url)
         my_html = uPage.read()
         uPage.close()
@@ -25,8 +19,11 @@ def getURL():
         pageParse = soup(my_html, 'html.parser')
         global myContainers
         myContainers = pageParse.find_all("div", {'class', 'responsive_search_name_combined'})
+        containerAmount +=len(myContainers)
+        getPNamePricePreview()
+        getPlatform()
+        getRating()
 
-        return myContainers
 
 
 def getPNamePricePreview():
@@ -66,9 +63,12 @@ def getPlatform():
        #-----------------------------------plate
     for m in myContainers:
         p2 = m.div.p
+        # print('--------',p)
         pl = []
         for i in p2:
             if len(i) != 1:
+                # print("------",i)
+                # print("--------", len(i))
                 plate = str(i)[26:29]
                 pl.append(plate)
         # print('>>>>',pl)
@@ -93,6 +93,8 @@ def getRating():
         if len(m) == 3:
 
             r = str(m).split(' ')
+            # print(r)
+            # print('))))',r)
             pr = r[6] + r[7] + r[8]
             p3 = pr[pr.index('%') - 2:pr.index('%') + 1]
             ratePercent_list.append(p3)
@@ -126,16 +128,8 @@ def writeData():
                 ratePercent_list[j]) + ',' + str(user_list[j]) + '\n')
             print(productName_list[j], plate_list[j], DropDown_list[j], oriPrice_list[j], nowPrice_list[j],
                   ratePercent_list[j], user_list[j])
-            print(j)
+        
         file.close()
-
-class textBox(Text):
-
-    def __init__(self, master=None):
-        super().__init__(master)
-
-        self.config()
-        self.insert(INSERT, '\n****************Data show here*************\n')
 
 class urlEntry(Entry):
     def __init__(self, master=None):
@@ -146,23 +140,64 @@ class urlEntry(Entry):
         self.insert(0, 'https://store.steampowered.com/search/?specials=1&page=')
 
 
-if __name__ == '__main__':
-    productName_list =[]
-    DropDown_list =[]
-    oriPrice_list=[]
-    nowPrice_list =[]
-    plate_list=[]
-    ratePercent_list=[]
-    user_list=[]
-    containerAmount = 0
+def getState():
+    inPage = v.get()
+    lb.insert(1, '----------------------------------------------')
+    getURL(inPage)
+    writeData()
+    disPlayData()
+
+
+
+def disPlayData():
+    lb.delete(0,END)
     
+    # in case some chinese character can't not be read
+    #.csv reading file and writing file can be the same name?
+    f = codecs.open("data2.csv", "r", "utf-8")
+    
+    spamreader = csv.reader(f, delimiter='|', quotechar='|')
+    for row in spamreader:
+        lb.insert(END, ',  '.join(row))
+    
+    print(len(productName_list), len(plateForm_list), len(DropDown_list), len(oriPrice_list), len(nowPrice_list),
+          len(ratePercent_list), len(user_list))
+
+
+#print(len(productName_list),len(plate_list),len(DropDown_list),len(oriPrice_list),len(nowPrice_list ),len(ratePercent_list),len(user_list))
+#print(containerAmount)
+if __name__ == '__main__':
     root = Tk()
     root.title(' My web scraper ')
-
+    frame = Frame(root)
+    v = IntVar()
+    pL = Label(root, text=' Range(<30)', width=10).grid(row=0, sticky='e')
+    uL = Label(root, text=' URL', width=10).grid(row=0, sticky='w')
     uE = urlEntry(root)
-    uL = Label(root, text=' URL', width=10).grid(row=2,sticky = 'w')
-    uPE = Entry(root, width=10).grid(row=2,column = 1, sticky = 'w')
-    uE.grid(row = 2, column = 0,sticky = 's')
-    tb = textBox(root)
-    tb.grid(row = 1)
+    uPE = Entry(root, width=10, textvariable = v)
+    uPE.grid(row=0,column = 1, sticky = 'w')
+    
+    uE.grid(row = 0, column = 0,sticky = 's')
+    lb = Listbox(root, width = 120)
+    lb.insert(1,'***************************Data show here****************')
+    lb.grid(row = 2,column = 0, rowspan = 2, sticky = 'nswe')
+    scoll = Scrollbar(root, orient = VERTICAL)
+    lb['yscrollcommand'] = scoll.set
+    scoll['command'] = lb.yview
+    scoll.grid(row = 2, column = 0, rowspan = 2, sticky = 'nse')
+    
+    Button(root, text='Go', command=getState).grid(row = 1,column = 1)
+    frame.grid(row = 1)
+    
+    productName_list = []
+    DropDown_list = []
+    oriPrice_list = []
+    nowPrice_list = []
+    plateForm_list = []
+    ratePercent_list = []
+    user_list = []
+    
+    containerAmount = 0
+    
+    
     root.mainloop()
